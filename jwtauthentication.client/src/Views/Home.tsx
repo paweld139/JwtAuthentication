134bcd090872b8ce3e243c1e57b7a2c126d7ea7b
paweld139 <pawell139139@gmail.com>
2024-05-29 20:37:01 +0200

    Clean

--- jwtauthentication.client/src/Views/Home.tsx
+++ jwtauthentication.client/src/Views/Home.tsx
@@ -1,18 +1,4 @@
-import { useEffect } from "react";
-
-import { useNavigate } from "react-router-dom";
-
 const Home = () => {
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-
-        if (!token) {
-            navigate('/login');
-        }
-    }, [navigate]);
-
     return (
         <div>
             <h1>Home</h1>
